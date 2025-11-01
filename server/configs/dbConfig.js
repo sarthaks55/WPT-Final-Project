1e134503aaf429b29c5c3f99c5804cfe815bb5d3
sarthaks55 <sarthak.sambare.ss@gmail.com>
2025-11-01 22:23:53 +0530

    Added db config

---
+++ server/configs/dbConfig.js
@@ -0,0 +1,24 @@
+import { createConnection } from "mysql2/promise";
+
+let conn = null;
+
+export async function connectDB() {
+  try {
+    conn = await createConnection({
+      host: "localhost",
+      user: "root",
+      password: "cdac",
+      port: 3306,
+      database: "yogadb",
+    });
+    console.log("DB connection created");
+  } catch (error) {
+    console.log("Error in db coneection");
+    console.log(error);
+  }
+  return conn;
+}
+
+export function getConnectionObject() {
+  return conn;
+}
