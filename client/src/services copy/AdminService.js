5c97e0acee852d07bceb2cd10404b9a1ea265f47
sarthaks55 <sarthak.sambare.ss@gmail.com>
2025-11-04 00:39:24 +0530

    Removed unwanted

--- client/src/services copy/AdminService.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-import { ADMIN_API_URL, API_BASE_URL } from "../constants/APIConstant";
-
-export function adminLogin(formData){
-    return axios.post(`${API_BASE_URL}/login`, formData)
-}
--- client/src/services copy/ContactServices.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-import { CONTACT_API_URL } from '../constants/APIConstant';
-
-export async function ContactUs(formData){
-    return axios.post(CONTACT_API_URL, formData);
-}
