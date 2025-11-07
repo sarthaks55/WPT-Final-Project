5f2fd96c2ebd001fb54a6bef80932d2419d55929
sarthaks55 <sarthak.sambare.ss@gmail.com>
2025-11-07 08:15:47 +0530

    changes

--- client/src/Components/NavigationBar.jsx
+++ client/src/Components/NavigationBar.jsx
@@ -1,45 +1,3 @@
-// import { Button, Container, Nav, Navbar } from "react-bootstrap";
-// import { LinkContainer } from "react-router-bootstrap";
-// import { useNavigate } from "react-router-dom";
-// import { removeToken } from "../services/TokenService";
-// import "../assets/css/NavBar.css";
-// import lotusLogo from "../assets/Images/lotus_logo_svg.svg";
-// import { getUsername, removeRoleID, removeUserID, removeUsername } from "../services/RoleNameService";
-// import Dropdown from 'react-bootstrap/Dropdown';
-// import DropdownButton from 'react-bootstrap/DropdownButton';
-// import { isLoggedIn } from "../services/TokenService";
-// import { InstructorNavBar } from "./Navigation bars/InstructorNavBar";
-// import { UserNavBar } from "./Navigation bars/UserNavBar";
-// import { AdminNavBar } from "./Navigation bars/AdminNavbar";
-// import { PublicNavBar } from "./Navigation bars/PublicNavBar";
-
-
-// export function Navigationbar() {
-
-//     const navigate = useNavigate();
-//     const username = getUsername();
-
-//     const Role_id = localStorage.getItem("role_id");
-//     const islogIn = isLoggedIn();
-
-
-//     return <PublicNavBar/>
-
-//     // if(!Role_id){
-//     //     return <PublicNavBar/>
-//     // }
-//     // else if(Role_id === "1"){
-//     //     return <AdminNavBar/>
-//     // }
-//     // else if(Role_id === "2"){
-//     //     return <InstructorNavBar/>
-//     // }
-//     // else if(Role_id === "3"){
-//     //     return <UserNavBar/>
-//     // }
-    
-// }
-
 import { Button, Container, Nav, Navbar } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import { removeToken } from "../services/TokenService";
@@ -103,38 +61,34 @@ export function Navigationbar() {
                             <Nav.Link className="nav-link-custom">About Us</Nav.Link>
                         </LinkContainer>
                         
-                        <LinkContainer to="/login">
-                            <Nav.Link className="nav-link-custom d-lg-none">login</Nav.Link>
-                        </LinkContainer>
-                        
                     </Nav>
                 </Navbar.Collapse>
                 <FontAwesomeIcon icon={faUser} className="me-1" style={{ color: 'white' }} />
                 {role_id === "3" ? (
 
-                <DropdownButton className="no-style-dropdown d-none d-lg-block" title={username}>
+                <DropdownButton className="no-style-dropdown" title={username}>
                     <Dropdown.Item href="/userdashboard">User Dashboard</Dropdown.Item>
-                    <Dropdown.Item onClick={handleLogout}>logout</Dropdown.Item>
+                    <Dropdown.Item onClick={handleLogout}>Logout</Dropdown.Item>
                 </DropdownButton>
                 
                 ) : role_id === "2" ? (
 
-                <DropdownButton className="no-style-dropdown d-none d-lg-block" title={username}>
+                <DropdownButton className="no-style-dropdown" title={username}>
                     <Dropdown.Item href="/instructorDashboard">Instructor Dashboard</Dropdown.Item>
-                    <Dropdown.Item onClick={handleLogout}>logout</Dropdown.Item>
+                    <Dropdown.Item onClick={handleLogout}>Logout</Dropdown.Item>
                 </DropdownButton>
                 
                 ) : role_id === "1" ? (
 
                 
-                <DropdownButton className="no-style-dropdown d-none d-lg-block" title={username}>
+                <DropdownButton className="no-style-dropdown" title={username}>
                     <Dropdown.Item href="/adminDashboard">Admin Dashboard</Dropdown.Item>
-                    <Dropdown.Item onClick={handleLogout}>logout</Dropdown.Item>
+                    <Dropdown.Item onClick={handleLogout}>Logout</Dropdown.Item>
                 </DropdownButton>
                 
                 ) : (
 
-                <Button className="Username-Button d-none d-lg-block"onClick={handleClick}>Login</Button>
+                <Button className="Username-Button"onClick={handleClick}>Login</Button>
                 
                 )}
 
